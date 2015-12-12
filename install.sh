230b4e9861e9d91dbc24342cf5b9d714a7141888
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-12-11 20:03:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,3 +21,16 @@
  update-alternatives --config java
 
  sudo gedit ~/.bashrc
+
+
+# Login as hadoopuser
+$ su - hadoopuser
+#Generate a ssh key for the user
+$ ssh-keygen -t rsa -P ""
+#Authorize the key to enable password less ssh 
+$ cat /home/hadoopuser/.ssh/id_rsa.pub >> /home/hadoopuser/.ssh/authorized_keys
+$ chmod 600 authorized_keys
+#Copy this key to slave-1 to enable password less ssh 
+$ ssh-copy-id -i ~/.ssh/id_rsa.pub slave-1
+#Make sure you can do a password less ssh using following command.
+$ ssh slave-1
