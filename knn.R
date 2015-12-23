55c24cbed0fe9218e7a22ad357d13fcd9c1613f6
rrigato <rrigato@horizon.csueastbay.edu>
2015-12-23 11:43:48 -0800

    Added input validation

--- knn.R
+++ knn.R
@@ -1,5 +1,13 @@
 library(MASS); library(ISLR)
 head(Smarket)
+
+x_variable = 1
+if (x_variable <0)
+{
+  print("Error: x_variable cannot be negative")
+}
+
+
 lda_model = lda(Direction~ Lag1 + Lag2, data = Smarket, subset = train)
 lda_model; plot(lda_model)
 lda_predict = predict(lda_model, Smarket.2005)
