d8c855b864bdb25e0cbca27fafbef8e2963ee136
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-12-22 23:41:50 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,16 @@
  cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
 
  wget -c http://mirror.olnevhost.net/pub/apache/hadoop/common/current/hadoop-2.6.0.tar.gz
+  sudo apt-get install ssh
+
+ sudo apt-get install rsync
+
+ ssh-keygen -t dsa -P ' ' -f ~/.ssh/id_dsa
+
+ cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
+
+ wget -c http://mirror.olnevhost.net/pub/apache/hadoop/common/current/hadoop-2.6.0.tar.gz
+
 
  sudo tar -zxvf hadoop-2.6.0.tar.gz
 
