251cf58a8ece9b9564f4c5eb75446dab161073f8
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-12-31 14:15:12 -0800

    Added Documentation, divided train randomly into train2 and test2

--- Telstra Model.R
+++ Telstra Model.R
@@ -1,3 +1,20 @@
+########################################################################################
+#Ryan Rigato copyright 2015 all rights reserved
+#Telstra Kaggle Competition
+#The goal of this script is to predict Telstra fault severity at any location.
+#The response variable is 0(no fault) 1 (a few faults)  or 2 (many faults)
+#
+#
+#
+#
+#
+#
+#
+#########################################################################################
+
+
+
+#importing the datasets that were provided by Telstra
 train <- read.csv("C:\\Users\\Randy\\Downloads\\Telstra Kaggle Competion\\train.csv")
 test <- read.csv("C:\\Users\\Randy\\Downloads\\Telstra Kaggle Competion\\test.csv")
 severity_type <- read.csv("C:\\Users\\Randy\\Downloads\\Telstra Kaggle Competion\\severity_type.csv")
@@ -5,6 +22,7 @@ resource_type <- read.csv("C:\\Users\\Randy\\Downloads\\Telstra Kaggle Competion
 log_feature <- read.csv("C:\\Users\\Randy\\Downloads\\Telstra Kaggle Competion\\log_feature.csv")
 event_type <- read.csv("C:\\Users\\Randy\\Downloads\\Telstra Kaggle Competion\\event_type.csv")
 
+#Making sure the data was inputed correctly
 head(train)
 head(test)
 head(severity_type)
@@ -20,8 +38,44 @@ class(train$location)
 
 
 nrow(train)
-.5*7381
-ran_num_train = sample(1:nrow(train),3690)
-test = 1:10; test2 = 1:5
 
+#edit The percentage of the dataset in the train2 and test2, used to build a model 
+size_of_train = floor(.5*nrow(train))
+ran_num_test = 1:nrow(train)
+
+#gets random numbers for train2 using a sample
+ran_num_train = sample(1:nrow(train), size_of_train)
+
+#numbers not randomly selected for train2 are included in test2
+#this command gets the numbers not in ran_num_train
+ran_num_test = ran_num_test[(!(ran_num_test %in% ran_num_train)) == TRUE]
 train2 = train[ran_num,]
+test2 = train[ran_num_test,]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
